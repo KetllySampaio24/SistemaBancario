c013a2f8c750a992ff60ac59cb9a30faad12b0cf
Kétlly Sampaio <ketlly.sampaio17@gmail.com>
2025-03-20 16:08:33 -0300

    Update ContaPoupanca.java

--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -1,19 +1,21 @@
 public class ContaPoupanca extends ContaBancaria {
 
     public double taxaDeJuros; // mensal
+    public double jurosAplicados;
 
     // Parâmetros
     public ContaPoupanca(String titular, double saldo, int senha, double taxaDeJuros) {
         super(titular, saldo, senha);
         this.taxaDeJuros = taxaDeJuros;
+        this.jurosAplicados = 0;
     }
 
 // Aplicar juros ao saldo
     public void aplicarJuros() {
         if (taxaDeJuros > 0) {
-            // Calcula os juros baseados no saldo
             double juros = getSaldo() * (taxaDeJuros / 100);
-            depositar(juros); // Adiciona os juros ao saldo
+            depositar(juros);
+            this.jurosAplicados = juros;
             System.out.println("Juros aplicados: R$ " + juros);
         } else {
             System.out.println("Taxa de juros inválida.");
@@ -26,5 +28,6 @@ public class ContaPoupanca extends ContaBancaria {
         super.emitirRelatorio(); // Chama o metodo mãe
         System.out.println("Tipo de Conta: Conta Poupança");
         System.out.println("Taxa de Juros: " + taxaDeJuros + "%");
+        System.out.println("Juros aplicados: R$" + jurosAplicados);
     }
 }
